eba97acf72b24bd97819443e061b37474c2a658e
YWijayaku <114077216+YWijayaku@users.noreply.github.com>
2024-02-08 15:39:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/chat/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { Suspense } from 'react'
+import { Flex } from '@radix-ui/themes'
+import { Chat, ChatContext, ChatSideBar, PersonaPanel, useChatHook } from '@/components'
+import PersonaModal from './PersonaModal'
+
+const ChatProvider = () => {
+  const provider = useChatHook()
+
+  return (
+    <ChatContext.Provider value={provider}>
+      <Flex style={{ height: 'calc(100% - 56px)' }} className="relative">
+        <ChatSideBar />
+        <div className="flex-1 relative">
+          <Chat ref={provider.chatRef} />
+          <PersonaPanel />
+        </div>
+      </Flex>
+      <PersonaModal />
+    </ChatContext.Provider>
+  )
+}
+
+const ChatPage = () => {
+  return (
+    <Suspense>
+      <ChatProvider />
+    </Suspense>
+  )
+}
+
+export default ChatPage
---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { Analytics } from '@vercel/analytics/react'
+import { Inter } from 'next/font/google'
+import { Toaster } from 'react-hot-toast'
+import { Header } from '@/components/Header'
+import ThemesProvider from '@/providers/ThemesProvider'
+import '@/styles/globals.scss'
+import '@/styles/theme-config.css'
+
+export const metadata = {
+  title: {
+    default: 'ChatGPT Lite',
+    template: `%s - ChatGPT Lite`
+  },
+  description: 'AI assistant powered by ChatGPT',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
+}
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemesProvider>
+          <Header />
+          {children}
+          <Toaster />
+        </ThemesProvider>
+        <Analytics />
+      </body>
+    </html>
+  )
+}
